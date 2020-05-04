143955ac4557369652eeb932ad27e8efc1d4233a
nimacle <51423924+nimacle@users.noreply.github.com>
2020-05-04 12:30:04 +0200

    graphcolor lien main
    création du lien avec la fonction de création de matrices

--- graphcolor.py
+++ graphcolor.py
@@ -6,7 +6,8 @@ Created on Sat Apr 18 22:54:55 2020
 import tkinter as tk
 import tkinter.font as tkFont
 import numpy as np
-
+import main
+'''
 ligne1=[0,1,0,0,0]
 ligne2=[0,0,0,0,0]
 ligne3=[0,0,1,0,0]
@@ -18,14 +19,19 @@ Matrice=np.array([ligne1,ligne2,ligne3,ligne4,ligne5])
 Matrice=Matrice.astype(str)
 Matrice=np.where(Matrice=="0",'black',Matrice)
 Matrice=np.where(Matrice=="1",'red',Matrice)
+'''
+
 
 class Application(tk.Frame):
     '''fenetre de l'application graphique'''
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.configure(background='#A0EBEF')
         self.pack()
         self.EP()
+        self.j=0
+        
     
     
     def delete_frame(self):
@@ -56,14 +62,20 @@ class Application(tk.Frame):
         self.accesES.grid(column=5,row=5)
         
         # Bouton pour quitter l'application
-        self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)  #bouton de fermeture (qui sert actuellement a rafraichir la page)
+        self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)#bouton de fermeture (qui sert actuellement a rafraichir la page)
         self.quit.grid(column=5,row=6)    #alignement de la page
     
     def ES(self):
         '''widgets de la Fenetre de selection du mode de visualisation'''
         self.n=self.population.get()
         self.R=self.RR.get()
+        self.list_j=main.Simulation(50,self.n)
+        for i in range(51):
+            self.list_j[i]=self.list_j[i].astype(str)
+            self.list_j[i]=np.where(self.list_j[i]=="0",'black',self.list_j[i])
+            self.list_j[i]=np.where(self.list_j[i]=="1",'red',self.list_j[i])
         self.delete_frame()
+        print
         
         #Titre fenetre de selection du mode de visualisation
         self.titrefenetre1= tk.Label(self,text="Menu de selection du mode de visualisation des données\n",font=tkFont.Font(size=20))
@@ -86,14 +98,14 @@ class Application(tk.Frame):
         
         
         self.a=int(900/self.n)  #taille d'une personne (carré)
-        '''
+        
         b=0
         c=0
         liste1=["red","blue","green","black","yellow"]*(int(self.n/5)+1)
         Matrice=[0]*self.n
         for i in range(self.n):
             Matrice[i]=liste1
-        '''
+        
         self.delete_frame()     #on vide la frame
         #canvas sur lequel est dessiné toutes les personnes (carré)
         self.canvas = tk.Canvas(self)    #création du canvas
@@ -102,11 +114,11 @@ class Application(tk.Frame):
         self.canvas.grid(column=0,columnspan=25,rowspan=25)    #alignement 
         for b in range(self.n):
             for c in range(self.n):
-                self.canvas.create_rectangle(b*self.a,c*self.a,(b*self.a)+self.a, (c*self.a)+self.a,fill=Matrice[b][c])    #création des personnes
+                self.canvas.create_rectangle(b*self.a,c*self.a,(b*self.a)+self.a, (c*self.a)+self.a,fill=self.list_j[0][b][c])    #création des personnes
 
 
         # Selecteur échelle qui permet de selectionner le jour
-        self.scale = tk.Scale(self, width=25,orient=tk.HORIZONTAL,length=200,from_=100000,to=999999)
+        self.scale = tk.Scale(self, width=25,orient=tk.HORIZONTAL,length=200,from_=0,to=50)
         self.scale.grid(row=11,column=26)
         
         # Bouton d'actualisation des couleurs
@@ -119,10 +131,14 @@ class Application(tk.Frame):
         
         
     def change(self):
+        
         '''actualisation de la fenetre en fonction des nouvelles valeurs''' #pour l'instant uniquement un choix de couleurs
+        
         for b in range(self.n):
             for c in range(self.n):
-                self.canvas.create_rectangle(b*self.a,c*self.a,(b*self.a)+self.a, (c*self.a)+self.a,fill="#"+str(self.scale.get()))
+                if app.list_j[self.j][b][c]!=app.list_j[self.scale.get()][b][c]:
+                    self.canvas.create_rectangle(b*self.a,c*self.a,(b*self.a)+self.a, (c*self.a)+self.a,fill=self.list_j[self.scale.get()][b][c])
+        self.j=self.scale.get()
         app.update()
         
         
@@ -130,4 +146,3 @@ class Application(tk.Frame):
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
-
