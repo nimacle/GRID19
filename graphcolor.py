af021a9e478a15e3e83224908616d07222cba282
nimacle <51423924+nimacle@users.noreply.github.com>
2020-05-28 13:12:53 +0200

    Update graphcolor.py
    waiting screen

--- graphcolor.py
+++ graphcolor.py
@@ -7,6 +7,7 @@ import tkinter as tk
 import tkinter.font as tkFont
 import numpy as np
 import main
+import time
 '''
 ligne1=[0,1,0,0,0]
 ligne2=[0,0,0,0,0]
@@ -54,35 +55,54 @@ class Application(tk.Frame):
         self.population.grid(row=2,column=1)
 
         #curseur de R(infectiosité)
-        self.Rtitle = tk.Label(self,text="R : ")
+        self.Rtitle = tk.Label(self,text="Infectiosité : ")
         self.Rtitle.grid(row=3,column=0)
         self.RR = tk.Scale(self, width=25,orient=tk.HORIZONTAL,length=200,from_=0,to=8,resolution=0.1)
         self.RR.grid(row=3,column=1)
+        
+        self.Mtitle = tk.Label(self,text="Mortalité : ")
+        self.Mtitle.grid(row=4,column=0)
+        self.MM = tk.Scale(self, width=25,orient=tk.HORIZONTAL,length=200,from_=0,to=1,resolution=0.01)
+        self.MM.grid(row=4,column=1)
 
         #bouton d'acces a la page de selection du mode de visualisation de l'epidémie
-        self.accesES = tk.Button(self, text="Simuler", fg="black",command=self.ES)
+        self.accesES = tk.Button(self, text="Simuler", fg="black",command=self.Ewait)
         self.accesES.grid(column=5,row=5)
 
         # Bouton pour quitter l'application
         self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)#bouton de fermeture (qui sert actuellement a rafraichir la page)
         self.quit.grid(column=5,row=6)    #alignement de la page
 
-    def ES(self):
-        '''widgets de la Fenetre de selection du mode de visualisation'''
+    def Ewait(self):
+        
         self.n=self.population.get()    #récupération dans deux variables de la population (racine de la population car cotée du carré)
         self.R=self.RR.get()
+        self.M=self.MM.get()
+        
+        self.delete_frame()
+        self.wait=tk.Label(self,text="Veuillez patienter",font=tkFont.Font(size=20))
+        self.wait.grid(row=0,column=0)
+        self.wait2=tk.Label(self,text="Simulation en cours",font=tkFont.Font(size=20))
+        self.wait2.grid(row=1,column=0)
+        self.update()
+        
         self.sain.set("Nombre de personnes saines au jour 0 : "+str(self.n**2-1))
-        self.list_j=main.Simulation(50,self.n,self.R)
-
+        self.list_j=main.Simulation(200,self.n,self.R,self.M)
+        
         # Convertiseur de chaque matrice de la liste en matrice de couleurs
-        for i in range(51):
+        
+        for i in range(201):
             self.list_j[i]=self.list_j[i].astype(str)
             self.list_j[i]=np.where(self.list_j[i]=="0",'black',self.list_j[i])
             self.list_j[i]=np.where(self.list_j[i]=="1",'red',self.list_j[i])
-
+            self.list_j[i]=np.where(self.list_j[i]=="2",'green',self.list_j[i])
+        
         self.delete_frame() #Supression des widgets pour la nouvelle fenêtre
-
-
+        
+        self.ES()
+        
+    def ES(self):
+        '''widgets de la Fenetre de selection du mode de visualisation'''
         #Titre fenetre de selection du mode de visualisation
         self.titrefenetre1= tk.Label(self,text="Menu de selection du mode de visualisation des données\n",font=tkFont.Font(size=20))
         self.titrefenetre1.grid(columnspan=3)
@@ -98,6 +118,8 @@ class Application(tk.Frame):
         # Bouton pour quitter l'application
         self.quit = tk.Button(self, text="QUIT", fg="red",command=self.master.destroy)  #bouton de fermeture (qui sert actuellement a rafraichir la page)
         self.quit.grid(column=3,row=4)    #alignement de la page
+        
+        self.update()
 
     def E1(self):
         ''''widgets de la fenetre de visualisation de l'épidémie qui se répend'''
@@ -129,7 +151,7 @@ class Application(tk.Frame):
         self.label_sain.grid(row=9, column=26)
 
         # Selecteur échelle qui permet de selectionner le jour
-        self.scale = tk.Scale(self, width=25,orient=tk.HORIZONTAL,length=200,from_=0,to=50)
+        self.scale = tk.Scale(self, width=25,orient=tk.HORIZONTAL,length=200,from_=0,to=200)
         self.scale.grid(row=11,column=26)
 
         # Bouton d'actualisation des couleurs
